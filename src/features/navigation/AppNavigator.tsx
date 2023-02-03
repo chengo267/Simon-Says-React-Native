1c65d3253303bf734f9cd3b151d7fe6933aca190
Chen Gohari <chengo267@gmail.com>
2023-02-03 23:24:09 +0200

    import

--- src/features/navigation/AppNavigator.tsx
+++ src/features/navigation/AppNavigator.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {navigationReadiness, navigationRef} from './RootNavigation';
-import HomeScreen from '../home/HomeScreen';
 import {RootStackParamList, Screens} from '../../constants/screens';
+import HomeScreen from '../Home/HomeScreen';
 
 const StackNavigator = createNativeStackNavigator<RootStackParamList>();
 const AppNavigator: React.FC = () => {
