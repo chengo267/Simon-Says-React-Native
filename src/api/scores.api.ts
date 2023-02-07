08fb630c2da752d20cfd314f75bf695022343929
Chen Gohari <chengo267@gmail.com>
2023-02-07 16:11:57 +0200

    added api calls

---
+++ src/api/scores.api.ts
@@ -0,0 +1,56 @@
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react';
+import {AddScoreReq, ScoreData} from '../models/simon.models';
+
+const baseQuery = fetchBaseQuery({baseUrl: 'https://locall.host/3000/'});
+const baseQueryWithErrorHandling: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error) {
+    console.log(result.error);
+    // showMessage({
+    //   message: i18n.t('error.somethingWentWrong'),
+    //   type: 'danger',
+    // });
+  }
+  return result;
+};
+
+export const scoresApi = createApi({
+  reducerPath: 'scoresApi',
+  baseQuery: baseQueryWithErrorHandling,
+  endpoints: builder => ({
+    getAllScores: builder.query<ScoreData[], void>({
+      query: () => 'scores',
+    }),
+    addScore: builder.mutation<ScoreData, AddScoreReq>({
+      query: body => ({
+        url: 'scores',
+        method: 'POST',
+        body,
+      }),
+    }),
+    deleteScore: builder.mutation<{status: string}, string>({
+      query(id: string) {
+        return {
+          url: `posts/${id}`,
+          method: 'DELETE',
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetAllScoresQuery,
+  useAddScoreMutation,
+  useDeleteScoreMutation,
+} = scoresApi;
--- src/models/simon.models.ts
+++ src/models/simon.models.ts
@@ -13,3 +13,17 @@ export const simonColorsStrings = {
   [SimonSaysColors.YELLOW]: COLORS.YELLOW,
   [SimonSaysColors.BLUE]: COLORS.BLUE,
 };
+
+export interface ScoreData {
+  id: string;
+  userId: string;
+  userName: string;
+  score: number;
+}
+
+export interface AddScoreReq {
+  createdAt: Date;
+  userId: string;
+  userName: string;
+  score: number;
+}
